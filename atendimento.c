160f66b73450ce60e177bef43a2c5775a3e1b492
Lucas <lucas@NotebookLucas.>
2024-12-08 11:48:32 -0300

    Mudancas da manha dia 8

--- atendimento.c
+++ atendimento.c
@@ -16,13 +16,59 @@ int prioridade;
 int tempo_atendimento;
 */
 
-struct cliente
+typedef struct cliente
 {
     int pid;
     int hora_chegada;
     int prioridade;
     int tempo_atendimento;
-};
+} cliente;
+
+//inicio da sessao fila
+typedef struct {
+    struct cliente fila[101];
+    int inicio, fim;
+} FilaCliente;
+
+//inicio simboliza a exata posicao do primeiro elemento
+//fim simboliza a exata posicao do proximo elemento a ser adicionado (algo como proximo do ultimo elemento da fila)
+void inicializarFila(FilaCliente* f) {
+    f->inicio = 0; // representa o inicio da fila
+    f->fim = 0; // representa o proximo a ser iniciado
+}
+
+/* vazia quando o inicio alcanca o fim, que ocorre quando o ultimo elemento da fila eh coletado, fazendo com que incremente inicio
+e fique na mesma posicao de fim, que eh onde o proximo elemento deve ser adicionado */
+int filaVazia(FilaCliente* f) {
+    return f->inicio == f->fim;
+}
+
+/* vazia quando o fim +  1 alcanca o inicio , que ocorre quando adicionamos um elemento de tal forma que sobre apenas um espaco vazio no vetor,
+que acabamos cedendo para que essa checagem ocorra corretamente (checagem de o espaco para o proximo elemento a ser adicionado ser o mesmo
+canto que o primeiro da fila esta) de tal forma que se diferencie da filaVazia*/
+int filaCheia(FilaCliente* f) {
+    
+    return (f->fim + 1) % 100 == f->inicio;
+}
+
+//insercao normal, verifica se esta cheia. Caso nao esteja, insere no fim e incrementa fim
+void inserir(FilaCliente* f, struct cliente c) {
+    if (!(filaCheia(f))) {
+        f->fila[f->fim] = c;
+        f->fim = (f->fim + 1) % 100;
+    }
+}
+
+//remocao normal, se nao tiver vazia, remove o primeiro da fila
+cliente remover(FilaCliente* f) {
+    if (!(filaVazia(f))) {
+        struct cliente c = f->fila[f->inicio];
+        f->inicio = (f->inicio + 1) % 100;
+        return c;
+    }
+}
+
+//fim da sessao fila
 
 
 /*criar o vetor de clientes, que vai ser a nossa fila 
@@ -30,26 +76,6 @@ a ideia da fila eh sempre aumentar o numero infinitamente, e a gente vai pegando
 os clientes na posicao do resto respectivo, o val_inicial == val_final, então a fila está vazia se val_final - val_inicial == 100, 
 entao a fila esta cheia
 */
-
-
-int is_empty(){
-
-}
-
-int is_full(){
-
-}
-
-
-
-
-void inserir_na_fila(int pid, int hora_chegada, int prioridade, int tempo_atendimento){
-    
-}
-
-void remover_da_fila(){
-
-}
 
 void* atendente(){
 
@@ -66,6 +92,10 @@ void* recepcao(int num_clientes, int tolerancia, int pid_1){
    sem_t* sem_atend =  sem_open("/sem_atend", O_CREAT, 0644, 1);   
    sem_t* sem_block = sem_open("/sem_block", O_CREAT, 0644, 1);   
 
+    int inicio, fim = 0;
+    
+    int hora_chegada;
+    int tempo_atendimento;
 
     pid_t pid_2;
     int i = 0;
@@ -75,7 +105,6 @@ void* recepcao(int num_clientes, int tolerancia, int pid_1){
         
         pid_2 = fork();
         i ++;
-        printf("pid_2: %d\n", pid_2);
 
         if (pid_2 < 0){
             printf("Não criou cliente\n");
@@ -84,39 +113,53 @@ void* recepcao(int num_clientes, int tolerancia, int pid_1){
 
         else if(pid_2 == 0){
 
-            printf("pid do cliente: %d\n", pid_2);
-            printf("Cliente %d criado!\n", i);
+            int pid_cliente = getpid();
+            printf("pid do cliente %d: %d\n", i, pid_cliente);
+            //printf("Cliente %d criado!\n", i);
 
             if (execl("./cliente", "cliente", (char *)NULL) == -1) {
                 perror("Erro ao executar cliente");
                 exit(EXIT_FAILURE);
             }
-
-            printf("Cliente %d adicionado na fila\n", i);
-            
-
         } else {
             // Processo pai
-            printf("pid do pai: %d\n", pid_1);
+            //printf("pid do pai: %d\n", pid_1);
+                        // se a fila nao estiver cheia 
+            /*while (is_full(inicio, fim) == 0){
+                usleep(100);
+            } */
+            
+            /*gerar prioridade
+            0: prioridade alta
+            1: prioridade baixa
+            */
+            int prioridade_cliente = rand() % 2;
+            //entra na fila
+            FILE* demanda = fopen("demanda.txt", "r");
+            fscanf(demanda, "%d", &tempo_atendimento);
+            fclose(demanda);
+            
+            // Agora tenta deletar o arquivo
+            if (remove("demanda.txt") == 0) {
+                printf("Arquivo 'demanda.txt' deletado com sucesso.\n");
+            } else {
+                printf("Erro ao deletar o arquivo 'demanda.txt'.\n");
+            }
+
+            //printf("tempo de atendimento %d\n", tempo_atendimento);
+
+            printf("pid_2: %d \n",pid_2);
+            
+            //inserir_na_fila(pid_2, hora_chegada,prioridade_cliente,tempo_atendimento);
+           
+            printf("Cliente %d adicionado na fila\n", i);
+            
             usleep(100000);
         }
+        
 
             
         }
-
-    // ler o arquivo de demanda para ver o tempo de atendimento do cliente    
-
-    /*gerar prioridade
-    0: prioridade alta
-    1: prioridade baixa
-    */
-    int prioridade_cliente = rand() % 2;
-
-    // adiciona o cara na fila com <pid, hora de chegada, prioridade_cliente, tempo_para atendimento>
-
-    /*
-    fila[0].pid = 1;    fila[0].hora_chegada = 1200;    fila[0].prioridade = 5;    fila[0].tempo_atendimento = 15;
-    */
         
     sem_close(sem_block);
     sem_close(sem_atend);
@@ -134,18 +177,16 @@ int main(int argc, char *argv[]){
     2 - tolerancia do cliente (X)
     */  
 
-   int num_clientes = atoi(argv[1]);
-   int tolerancia = atoi(argv[2]);
+    int num_clientes = atoi(argv[1]);
+    int tolerancia = atoi(argv[2]);
+    
     printf("numero de clientes %d\n", num_clientes);
-  
+    
     pid_t pid = getpid();
     printf("pid do atendimento: %d\n", pid);
-    /*
-    aqui poderia criar a struct da fila, ou pelo menos declarar ela, caso seja so um vetor, 
-    ela tem que ter aquele formato <PID, Hora de chegada, Prioridade, Tempo para atendimento> para cada posicao
-    */
-
-    struct cliente fila[100];
+    
+    
+    
 
     pthread_t thread_1, thread_2;
     pthread_create(&thread_1, NULL, atendente, NULL);
